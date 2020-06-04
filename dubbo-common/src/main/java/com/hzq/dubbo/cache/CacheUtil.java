3ceb3b325770cd56dd75c5919697658c98ad8454
huangzq <huangzhenqiang@rainbowcn.com>
2020-06-04 10:53:07 +0800

    实例修改

--- dubbo-common/src/main/java/com/hzq/dubbo/cache/CacheUtil.java
+++ dubbo-common/src/main/java/com/hzq/dubbo/cache/CacheUtil.java
@@ -3,18 +3,20 @@ package com.hzq.dubbo.cache;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.function.Supplier;
 
 /**
- * 功能说明
+ * 缓存操作
  *
  * @author 黄震强
  * @version 1.0.0
  * @date 2020/6/4 9:39
  */
 @SuppressWarnings("unchecked")
+@Component
 public class CacheUtil {
     /**
      * 获取缓存时设置缓存
